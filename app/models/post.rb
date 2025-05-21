125b3f862f9b3e06edb452ec06ab16d2e823b88a
mmeest <martin.maasik.001@mail.ee>
2025-05-21 17:43:25 +0300

    Added styling and menus

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
+  validates :keywords, presence: true, length: { minimum: 1, maximum: 100 }
+end
