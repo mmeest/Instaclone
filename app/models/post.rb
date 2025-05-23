bc0fee53c5645877842ff52bb483130ac50b35ca
mmeest <martin.maasik.001@mail.ee>
2025-05-23 11:18:40 +0300

    fixed lint error

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 5, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
   validates :keywords, presence: true, length: { minimum: 1, maximum: 100 }
-  
+
   has_many_attached :images
 end
